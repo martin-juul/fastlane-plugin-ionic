1ec7e8b58ed9395d75f2873f7a510ef58592de64
Andrea Fortunato <fortu42@users.noreply.github.com>
2020-07-05 15:45:13 +0200

    change string type from all args

--- lib/fastlane/plugin/ionic/actions/ionic_action.rb
+++ lib/fastlane/plugin/ionic/actions/ionic_action.rb
@@ -43,7 +43,7 @@ module Fastlane
           # handle all other cases
           else
             unless param_value.to_s.empty?
-              platform_args << "--#{cli_param}=#{param_value.shellescape}"
+              platform_args << "--#{cli_param}=#{param_value.to_s.shellescape}"
             end
           end
         end
